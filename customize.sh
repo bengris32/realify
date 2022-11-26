140e7c5cc70623c5a53c48c49995e62625fe60f7
techyminati <sinha.aryan03@gmail.com>
2022-11-26 16:16:42 +0530

    realify: Support realmeUI 2.0 (Android 11)
    Change-Id: Ie6c2eda91b1a3548fc66f4ed241adaca74e67464
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-MINAPI=31
+MINAPI=30
 MAXAPI=32
 #DYNLIB=true
 #DEBUG=true
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,13 +2,24 @@
 
 # TODO: Is this correct?
 PRJ_NAME=$(getprop ro.oplus.image.my_product.type)
-FEATURE_PATH="/my_product/etc/extension/realme_product_rom_extend_feature_${PRJ_NAME}.xml"
+RUI_VER=$(getprop ro.build.version.realmeui)
 TEMP_PATH="/cache/feature.xml"
 
 # Functions 
 remove_feature()
 {
     sed -i "/${1}/d" $TEMP_PATH
+}
+
+check_realmeui_ver()
+{
+    if [[ $RUI_VER == V2.0 ]]; then
+        FEATURE_PATH="/my_product/etc/extension/appfeature_liteos.xml"
+    elif [[ $RUI_VER == V3.0 ]]; then
+        FEATURE_PATH="/my_product/etc/extension/realme_product_rom_extend_feature_${PRJ_NAME}.xml"
+    else
+        echo "Error"
+    fi
 }
 
 prepare_feature_list() 
@@ -19,14 +30,25 @@ prepare_feature_list()
 
 remove_lowend_features()
 {
-    # Enables high end app launch animations
-    remove_feature com.android.launcher.light_animator
-    # Enables blur in the majority of the UI
-    remove_feature com.android.systemui.gauss_blur_disabled
-    # Enables launcher card
-    remove_feature com.android.launcher.card_disabled
-    # Disables "lightos" sound recorder
-    remove_feature com.oplus.soundrecorder.lightos
+    if [[ $RUI_VER == V2.0 ]]; then
+        # Enables high end app launch animations
+        remove_feature com.oppo.launcher.light_animator
+        # Enables blur in the majority of the UI
+        remove_feature com.android.systemui.gauss_blur_disabled
+        # Disables "lightos" QS tiles
+        remove_feature com.android.systemui.apply_light_os_qs_tile
+    elif [[ $RUI_VER == V3.0 ]]; then
+        # Enables high end app launch animations
+        remove_feature com.android.launcher.light_animator
+        # Enables blur in the majority of the UI
+        remove_feature com.android.systemui.gauss_blur_disabled
+        # Enables launcher card
+        remove_feature com.android.launcher.card_disabled
+        # Disables "lightos" sound recorder
+        remove_feature com.oplus.soundrecorder.lightos
+    else
+        echo "Error"
+    fi
 }
 
 setup_mount()
@@ -36,6 +58,7 @@ setup_mount()
 }
 
 # Run these in order
+check_realmeui_ver
 prepare_feature_list
 remove_lowend_features
 setup_mount
