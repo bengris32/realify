774295d8efed30d4f1f4914d90453ca2697ef6f2
techyminati <sinha.aryan03@gmail.com>
2022-11-26 18:33:35 +0530

    realify: Support API 29 - 33 * We support realmeUI 1.0 (A10) and realmeUI 4.0 (A13)
    Change-Id: I505033543db772de1ebb99940f04562a704f8f8f
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- customize.sh
+++ customize.sh
@@ -11,8 +11,8 @@
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-MINAPI=30
-MAXAPI=32
+MINAPI=29
+MAXAPI=33
 #DYNLIB=true
 #DEBUG=true
 
