2434e951f07d4ac7b29f52b08e8718217306d043
Aryan Sinha <sinha.aryan03@gmail.com>
2024-04-10 14:59:32 +0530

    realify: Handle realmeUI 5.0

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -31,6 +31,8 @@ check_realmeui_ver()
         FEATURE_PATH="/my_product/etc/extension/realme_product_rom_extend_feature_${PRJ_NAME}.xml"
     elif [[ $RUI_VER == V4.0 ]]; then
         FEATURE_PATH="/my_product/etc/extension/feature_com.coloros.oppoguardelf.xml"
+    elif [[ $RUI_VER == V3.0 ]]; then
+        FEATURE_PATH="/my_product/etc/extension/realme_product_rom_extend_feature_${PRJ_NAME}.xml"
     elif [[ $RUI_VER == V1.0 ]]; then
         FEATURE_PATH="/oppo_product/etc/permissions/com.oppo.features.os.xml"
     else
